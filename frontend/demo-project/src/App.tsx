24a4c3b6f5da81c0181fdd00f19e5b29c5dca602
Palaparthi  Jitendranadh (Ext) <jiten.p.ext@omnenest.com>
2025-08-29 15:46:04 +0530

    add more examples

---
+++ frontend/demo-project/src/App.tsx
@@ -0,0 +1,18 @@
+import Demo from './components/demo.tsx'
+import Logo from './components/logo.tsx'
+import Card from './components/card.tsx'
+import Misc from './components/misc.tsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Logo></Logo>
+     <Demo></Demo>
+     <Card></Card>
+     <Misc></Misc>
+    </>
+  )
+}
+
+export default App
