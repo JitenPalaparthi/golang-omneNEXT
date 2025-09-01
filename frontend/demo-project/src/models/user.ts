00804b31f5251732027b86d22376e6c53422637d
Palaparthi  Jitendranadh (Ext) <jiten.p.ext@omnenest.com>
2025-09-01 14:55:26 +0530

    add more examples

--- frontend/demo-project/src/models/user.ts
+++ frontend/demo-project/src/models/user.ts
@@ -1,5 +1,11 @@
-export type User = {
+export interface User  {
   name: string;
-  email?: string; // optional field
+  email?: string ; // optional field
   contact: string;
 };
+
+// difference between type and interface 
+// ? filed .. what is type inference, undefined , null
+// what is a union in type script 
+// what is called a record in typescript
+// grid vs flex vs classical div vs tables 
---
+++ frontend/dummy-json-users-demo/src/pages/RegistrationPage.tsx
@@ -0,0 +1,6 @@
+export default function RegistrationPage(){
+return (
+<>
+<h1> This is a User registration Page</h1>
+</>)
+}
